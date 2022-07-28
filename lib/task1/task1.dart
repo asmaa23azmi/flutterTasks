a2a520d5e1c577aeae21b2cd2c1614663535e698
asmaa23azmi <azmiasmaa23@gmail.com>
2022-07-28 12:10:53 +0300

    Flutter

---
+++ lib/task1/task1.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class task1 extends StatelessWidget {
+  const task1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Log in',
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 25.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      hintText: 'Email address',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 2.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 25.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      hintText: 'Password',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 2.0,
+                ),
+                GestureDetector(
+                  onTap: () {
+//do something
+                    print('LogIn Button');
+                  },
+                  child: Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    height: 60.0,
+                    child: Center(
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(
+                          fontFamily: 'Open Sans',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.cyan[800],
+                        borderRadius: BorderRadius.circular(30.0)),
+                  ),
+                ),
+                SizedBox(
+                  height: 2.0,
+                ),
+                Text(
+                  'Forgot password?',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextButton(
+                  onPressed: () {
+//do something for example
+                    print('Register Here Button');
+                  },
+                  child: Text(
+                    "Register Here",
+                    style: TextStyle(
+                      fontFamily: 'Open Sans',
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
